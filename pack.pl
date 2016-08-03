79dfe195afbc9c6e1d5fd8999ab3a0786b26342b
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-08-03 16:12:50 +0200

    Added metadata

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(space).
+version('0.1').
+title('Space package').
+keywords(['geo']).
+author( 'Willem van Hage', 'w.vanhage@esciencecenter.nl' ).
+packager( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+maintainer( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+home( 'http://www.swi-prolog.org' ).
+download( 'http://www.swi-prolog.org/download/space-*.tgz' ).
