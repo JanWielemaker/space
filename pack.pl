f08af7bacdbb7c3be8b45b88163d27d395341a90
Peter Ludemann <peter.ludemann@gmail.com>
2024-07-08 17:57:02 -0700

    Use pack version 2 variables

--- pack.pl
+++ pack.pl
@@ -7,3 +7,5 @@ packager( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
 maintainer( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
 home( 'https://github.com/JanWielemaker/space.git' ).
 download( 'https://github.com/JanWielemaker/space/archive/V*.zip' ).
+pack_version(2).
+% prolog_version('9.3.8').
