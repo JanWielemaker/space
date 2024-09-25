5ecde4e35fca63c94188a1a6397510b71aab0077
Jan Wielemaker <jan@swi-prolog.org>
2024-09-25 19:37:53 +0200

    Updated pack.pl

--- pack.pl
+++ pack.pl
@@ -6,8 +6,6 @@ author( 'Willem van Hage', 'w.vanhage@esciencecenter.nl' ).
 packager( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
 maintainer( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
 home( 'https://github.com/JanWielemaker/space.git' ).
-download( 'https://github.com/JanWielemaker/space/archive/V*.zip' ).
+download( 'https://github.com/JanWielemaker/space.git' ).
 pack_version(2).
-% requires(prolog >= "9.3.8"). % TODO: stable version
 requires(prolog:c_cxx(_)).
-
